2db6f2d8433d5bed94b388e4b7ce0101edcafd90
Ashish Kaintura <ashu88793@gmail.com>
2025-12-31 15:24:58 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FirstClassFlight from './FirstClassFlight'
+
+export default function Home() {
+  return (
+    <div>
+      {/* <FirstClassFlight/> */}
+    </div>
+  )
+}
